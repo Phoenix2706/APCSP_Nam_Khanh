a4b63180d8485fa9db683acb3744d208e2bdc87c
Phoenix2706 <Nam>
2023-04-09 22:30:01 +0700

    second commit

--- AP RPS.py
+++ AP RPS.py
@@ -1,5 +1,5 @@
 import random
-# import the function "random" to use later when deciding computer's hand
+# import the function "random" to use when deciding computer's hand
 
 user_score = 0
 computer_score = 0
@@ -8,12 +8,12 @@ play_count = 0
 
 
 def play_game():
-    user_hand = input("please choose: (rock/paper/scissors) ").lower()
-    # receives input as user's playhand
+    user_hand = input("Choose your weapon: rock, paper, or scissors? ").lower()
+    # receives input as user's hand
     outcomes = ["rock", "paper", "scissors"]
     computer_hand = random.choice(outcomes)
 
-    # set the computer to randomly choose rock, paper, or scissors
+    # values rock, paper, or scissors is chosen at random to determine computer's hand
     return [user_hand, computer_hand]
 
 
@@ -52,12 +52,12 @@ def show_result(user_input, computer_input):
         winner = 2
 
     return winner
-# return the value "winner" to increment either the user's score or the computer's score.
+# the returned "winner" value is used to determine which side the score should be given to.
 
 
 print("Hello and welcome to Rock, Paper, Scissors!")
 user_name = input("Please enter your username: (example: Khanh) ").upper()
-play_count_request = int(input("How many times would you like to play? "))
+play_count_request = int(input("Alright " + user_name + "! How many times would you like to play? "))
 # receive the user's requested play count
 while play_count < play_count_request:
     user_hand_check, computer_hand_check = play_game()
@@ -84,5 +84,12 @@ while play_count < play_count_request:
         print("--------------------FINAL SCOREBOARD---------------------")
         print(user_name + ":" + str(user_score))
         print("MOUNTAIN DEW-BOT: " + str(computer_score))
-        print("Thank you for playing Rock, Paper, Scissors!")
+        if user_score > computer_score:
+            print("Nice job, you've won!")
+        if user_score < computer_score:
+            print("You've lost, maybe try again next time?" )
+        if user_score == computer_score:
+            print("That's a tie! Well played!")
+        # display the winner based on the final scoreboard
+        print("Thank you for playing Rock, Paper, Scissors! ")
         break
